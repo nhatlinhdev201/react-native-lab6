fd004fb15556634fecbac5957e8a3db94a201c10
nhatlinh <nhatlinhdev201@gmail.com>
2023-10-20 08:46:46 +0700

    customize Ui navigation

--- App.js
+++ App.js
@@ -1,17 +1,64 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
 import Screen1 from './src/screen/Screen1';
 import Screen2 from './src/screen/Screen2';
 
 const Stack = createNativeStackNavigator();
 export default function App() {
-    return ( 
+    return (
         <NavigationContainer>
             <Stack.Navigator initialRouteName='Screen1'>
-                <Stack.Screen name='Screen1' component= {Screen1} options={{headerShown : false}}/>
-                <Stack.Screen name='Screen2' component={Screen2}/>
+                <Stack.Screen
+                    name='Screen1'
+                    component={Screen1}
+                    options={{ headerShown: false }}
+                />
+                <Stack.Screen
+                    name='Screen2'
+                    component={Screen2}
+                    options={{
+                        headerTitle: 'title',
+                        headerLeft: () => (
+                            <Pressable
+                                onPress={() => navigation.navigate('Profile')}
+                                style={{
+                                   flexDirection : 'row',
+                                   justifyContent : 'center',
+                                   alignItems : 'center'
+                                }}
+                            >
+                                <ArrowBackIcon />
+                                <Text>Back</Text>
+                            </Pressable>
+                        ),
+                        headerTitle: () => (
+                            <Text
+                                style={{
+                                    fontSize: 18,
+                                    fontWeight: 'bold',
+                                    textAlign : 'center',
+                                }}>
+                                Chi tiết
+                            </Text>
+                        ),
+                        headerRight: () => (
+                            <Pressable
+                                onPress={() => {}}
+                                style={{
+                                    flexDirection : 'row',justifyContent : 'center',
+                                    alignItems : 'center'
+                                }}
+                            >
+                                <Text>Next</Text>
+                                <ArrowForwardIcon />
+                            </Pressable>
+                        ),
+                    }}
+                />
             </Stack.Navigator>
         </NavigationContainer>
     );
